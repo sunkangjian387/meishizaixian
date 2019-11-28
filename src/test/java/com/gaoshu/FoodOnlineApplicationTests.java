b638caf1340cf5374b3daa28c93e1bd1daac9df4
dell <1477952071@qq.com>
2019-11-28 16:55:23 +0800

    提交test目录

---
+++ src/test/java/com/gaoshu/FoodOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaoshu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
