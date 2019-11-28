b61698108d9a867ca58687f81c30422cba846fdd
dell <1477952071@qq.com>
2019-11-28 17:19:35 +0800

    修改注释

--- src/main/java/com/gaoshu/FoodOnlineApplication.java
+++ src/main/java/com/gaoshu/FoodOnlineApplication.java
@@ -2,8 +2,12 @@ package com.gaoshu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.gaoshu.mapper")
+@EnableCaching
 public class FoodOnlineApplication {
 
     public static void main(String[] args) {
