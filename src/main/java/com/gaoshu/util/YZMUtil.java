ed3bcd253add9acdca6ce7934aa7d831d60b119d
sunkangjian387 <1477952071@qq.com>
2019-11-29 15:52:09 +0800

    验证码获取

--- src/main/java/com/gaoshu/util/YZMUtil.java
+++ src/main/java/com/gaoshu/util/YZMUtil.java
@@ -1,4 +1,12 @@
 package com.gaoshu.util;
 
 public class YZMUtil {
+    public static String getYZM(){
+       String a="";
+       for (int i=0;i<6;i++){
+           double uzm= Math.random()*10;
+           a+=(int)uzm;
+        }
+        return  a;
+    }
 }
