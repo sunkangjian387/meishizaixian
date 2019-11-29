abfadf1e1b5e0770fb987172221afee786d2d902
sunkangjian387 <1477952071@qq.com>
2019-11-29 10:20:14 +0800

    Service添加Service注解

--- src/main/java/com/gaoshu/service/serviceimpl/YHserviceImpl.java
+++ src/main/java/com/gaoshu/service/serviceimpl/YHserviceImpl.java
@@ -1,7 +1,9 @@
 package com.gaoshu.service.serviceimpl;
 
 import com.gaoshu.service.YHService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class YHserviceImpl implements YHService {
 
 }
